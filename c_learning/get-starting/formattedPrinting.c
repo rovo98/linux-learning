df3253a2fb898da1b50a70f49b7f55cea282f5c6
rovo98 <2402323937@qq.com>
2018-03-11 00:03:32 +0800

    Update formattedPrinting.c

--- c_learning/get-starting/formattedPrinting.c
+++ c_learning/get-starting/formattedPrinting.c
@@ -11,9 +11,9 @@
 int main(int argc, char *argv[])
 {
 	int age = 10; 
-	int height;
+	int height = 72;
 
-	printf("I am %d years old.\n");
+	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
 
 	return 0;
