c3cff2c5da567ce593c36fb7f62ac23671a80b9a
rovo98 <2402323937@qq.com>
2018-03-11 00:03:09 +0800

    Create formattedPrinting.c

---
+++ c_learning/get-starting/formattedPrinting.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*********************************************
+ * A demo to use formatted printing.
+ *
+ * author : rovo98
+ * Date: 2018.3.10
+ * *******************************************/
+
+
+int main(int argc, char *argv[])
+{
+	int age = 10; 
+	int height;
+
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
+}
