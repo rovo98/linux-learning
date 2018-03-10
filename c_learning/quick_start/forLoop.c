0f1d1036be01a6b8df9c97da68b9ce08e1f491d2
rovo98 <2402323937@qq.com>
2018-03-10 17:45:17 +0800

    Create forLoop.c

---
+++ c_learning/quick_start/forLoop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*******************************************
+* for loop demo. 
+* percent complete demo.
+********************************************/
+
+
+int main(int argc, int *argv[])
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		printf("Percent complete: %d\%\r", i);
+		fflush(stdout);
+		sleep(1);
+	}
+	printf("\n");
+	return 0;
+}
+
