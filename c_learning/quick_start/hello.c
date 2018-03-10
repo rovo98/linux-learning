a3ace0b4c6578df9368f4294adbc13f282a2f639
rovo98 <2402323937@qq.com>
2018-03-10 17:43:28 +0800

    Create hello.c

---
+++ c_learning/quick_start/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**********************************
+ *
+ * A program to print "hello, world"
+ * to the stdout.
+ *
+ *
+ * author : rovo98
+ *
+ * **********************************/
+
+
+int main(int argc, char *argv[]) {
+	printf("hello, world\n");
+	return 0;
+}
