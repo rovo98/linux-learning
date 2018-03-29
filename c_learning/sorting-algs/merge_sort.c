b614b6a2ba262e7aabf11d5e0a11cf986b98b7c6
rovo98 <2402323937@qq.com>
2018-03-29 18:20:58 +0800

    update merge_sort.c

--- c_learning/sorting-algs/merge_sort.c
+++ c_learning/sorting-algs/merge_sort.c
@@ -1,3 +1,4 @@
+#include "merge.h"
 #include <stdio.h>
 
 /***********************************
@@ -11,37 +12,6 @@
  * author : rovo98
  *
  * *********************************/
-// merge method for mergeSort.
-void merge(int a[], int aux[], int lo, int mid, int hi)
-{
-	int i = lo;
-	int j = mid + 1;
-	int k;
-
-	for (k = lo; k <= hi; k++)
-		aux[k] = a[k];
-
-	for (k = lo; k <= hi; k++)
-	{
-		if (j > hi)
-			a[k] = aux[i++];
-		else if (i > mid)
-			a[k] = aux[j++];
-		else if (aux[j] < aux[i])
-			a[k] = aux[j++];
-		else 
-			a[k] = aux[i++];
-	}
-}
-void sort(int a[], int aux[], int lo, int hi)
-{
-	int mid;
-	if (hi <= lo) return;
-	mid = lo + (hi - lo) / 2;
-	sort(a, aux, lo, mid);
-	sort(a, aux, mid+1, hi);
-	merge(a, aux, lo, mid, hi);
-}
 
 // Driver the program to test the methods above.
 int main(int argc, char *argv[])
@@ -59,7 +29,7 @@ int main(int argc, char *argv[])
 		printf("%d ", a[i]);
 	printf("\n");
 
-	sort(a, aux, 0, len-1);
+	sort(a, aux, len);
 	printf("After calling mergeSort function, it becomes:\n");
 	for (i = 0; i < len; i++)
 		printf("%d ", a[i]);
