90c7c468156b1fd5fbb081ebda2ab5915c939ff5
rovo98 <2402323937@qq.com>
2018-03-29 18:22:21 +0800

    add bubble.h

---
+++ c_learning/sorting-algs/bubble.h
@@ -0,0 +1,11 @@
+
+#define true 1
+#define false 0
+
+/*******************************************
+ * Sorting tool functions
+ * ****************************************/
+
+void sort(int a[], int len);
+void exch(int a[], int i, int j);
+
