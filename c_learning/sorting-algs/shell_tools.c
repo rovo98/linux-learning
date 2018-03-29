12276eca16c51d192e89a3868f69a6ab3a3667ba
rovo98 <2402323937@qq.com>
2018-03-29 18:26:23 +0800

    add shell_tools.c

---
+++ c_learning/sorting-algs/shell_tools.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+#include <stdio.h>
+
+void sort(int a[], int len) {
+	int i, j, key;
+	int h = 1;
+	while (h < len)
+		h = 3 * h + 1; // 1, 4, 13, 40, 121, ...
+	while (h >= 0) {
+		for (i = h; i < len; i++) {
+			key = a[i];
+			j = i - h;
+			while (j >= 0&&key < a[j]) {
+				a[j+h] = a[j];
+				j -= h;
+			}
+			a[j+h] = key;
+		}
+		h /= 3;
+	}
+}
