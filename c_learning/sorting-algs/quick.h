9f45759d96dc83b7a3aefa9e322ee162d35e23b5
rovo98 <2402323937@qq.com>
2018-03-29 18:24:34 +0800

    add quick.h

---
+++ c_learning/sorting-algs/quick.h
@@ -0,0 +1,15 @@
+
+
+/************************************
+ *
+ * header file for quick sort
+ *
+ * author : rovo98
+ * ***********************************/
+
+#define true 1
+#define false 0
+
+void exch(int a[], int i, int j);
+void sort(int a[], int len);
+void quick_sort(int a[], int lo, int hi);
