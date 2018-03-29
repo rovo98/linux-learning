58cddfc272a42dc439ea9c80b1109ceb28e7a145
rovo98 <2402323937@qq.com>
2018-03-29 18:25:05 +0800

    add selection.h

---
+++ c_learning/sorting-algs/selection.h
@@ -0,0 +1,9 @@
+
+/*********************************************
+ * Header file for selection sort.
+ *
+ * author : rovo98
+ * ********************************************/
+
+void exch(int a[], int i, int j);
+void sort(int a[], int len);
