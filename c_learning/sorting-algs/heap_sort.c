99ad28ffe85891d854f4d418eb42ca36d89acedd
rovo98 <2402323937@qq.com>
2018-03-29 18:20:16 +0800

    update heap_sort.c

--- c_learning/sorting-algs/heap_sort.c
+++ c_learning/sorting-algs/heap_sort.c
@@ -1,3 +1,4 @@
+#include "heap.h"
 #include<stdio.h>
 
 /************************************
@@ -8,38 +9,7 @@
 
 
 // This is a demo for heapSort.
-/*************************************
- * helper for sorting.
- * ***********************************/
-void exch(int a[], int i, int j) {
-	int temp = a[i];
-	a[i] = a[j];
-	a[j] = temp;
-}
-// sink operation.
-void sink(int a[], int k, int n) {
-	while (2 * k < n) {
-		int j = 2 * k;
-		if (j < n-1 && a[j] < a[j+1])
-			j++;
-		if (a[k] >= a[j])
-			break;
-		exch(a, k, j);
-		k = j;
-	}
-}
-// heap sort.
-void sort(int a[], int len) {
-	int k;
-	int n = len;
-	for (k = n/2; k>=0; k--) {
-		sink(a, k, n);
-	}
-	while (n >= 0) {
-		exch(a, 0, --n);
-		sink(a, 0, n);
-	}
-}
+
 // Driver the method to test the methods above.
 int main(int argc, char *argv[])
 {
