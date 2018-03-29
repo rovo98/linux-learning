7ac5403f494fa939bb48abacd6732fa52c5c8d1a
rovo98 <2402323937@qq.com>
2018-03-29 18:22:48 +0800

    add insertion.h

---
+++ c_learning/sorting-algs/insertion.h
@@ -0,0 +1,10 @@
+
+
+/*****************************************
+ *
+ * header file for insertion sort
+ *
+ * author : rovo98
+ * ***************************************/
+
+void sort(int a[], int len);
