4d31bd7e4f14c89d42dd82f8fb0cc717fcb6894a
rovo98 <2402323937@qq.com>
2018-03-29 18:22:34 +0800

    add heap.h

---
+++ c_learning/sorting-algs/heap.h
@@ -0,0 +1,15 @@
+
+
+/**************************************************************
+ *
+ * header file for heap sort.
+ *
+ *
+ * author : rovo98
+ *
+ * ***********************************************************/
+
+
+void sort(int a[], int len);
+void sink(int a[], int k, int len);
+void exch(int a[], int i, int j);
