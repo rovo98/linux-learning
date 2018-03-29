c86c9649373c3632ba7552861733cc1fd5568804
rovo98 <2402323937@qq.com>
2018-03-29 18:19:12 +0800

    update sizes_and_arrays.c

--- c_learning/get-starting/sizes_and_arrays.c
+++ c_learning/get-starting/sizes_and_arrays.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	// WARNING : On some systems you may have to change the 
 	// %ld in this code to a %u since it will use unsigned ints
-	printf("The size of an int: %d\n", sizeof(int));
+	printf("The size of an int: %ld\n", sizeof(int));
 	printf("The size of areas (int[]): %ld\n", sizeof(areas));
 	printf("The number of ints in areas: %ld\n",
 			sizeof(areas) / sizeof(int));
