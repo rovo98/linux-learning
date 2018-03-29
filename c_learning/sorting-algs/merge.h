82a2136c917d84e5802880ae8763f5998b44732c
rovo98 <2402323937@qq.com>
2018-03-29 18:23:59 +0800

    add merge.h

---
+++ c_learning/sorting-algs/merge.h
@@ -0,0 +1,15 @@
+
+
+
+/******************************************************
+ * header file for merge sort.
+ *
+ *
+ * author : rovo98
+ *
+ * ****************************************************/
+
+
+void sort(int a[], int aux[], int len);
+void merge_sort(int a[], int aux[], int lo, int hi);
+void merge(int a[], int aux[], int lo, int mid, int hi);
